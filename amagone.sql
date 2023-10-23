94f9abcc202e5c4f8cd0ad430f05da9d278a023f
Remi P <perez.remi@gmail.com>
2023-10-23 14:46:53 +0200

    feat : modif soft

--- amagone.sql
+++ amagone.sql
@@ -2,9 +2,7 @@
 Modification depuis le distant
 Je Fais un essai pour merger des branches
 
-zizi
 
-Là je suis sur branche1
 -------------------------------------------
 Le magasin AMAGONE vend des produits en dropshipping, où plusieurs vendeurs peuvent proposer des produits pré-existants, au prix qu'ils souhaitent et en renseignant leur stock disponible.
 Chaque client peut passer commande d'une ou plusieurs offres ('propal'), en choisissant pour chacune un mode de livraison proposé parmis une liste.
